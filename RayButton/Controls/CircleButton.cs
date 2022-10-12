6e12bb9195625660c4a3547c488bf2256c144434
雷瀚琳 <leihanlin@megarobo.tech>
2022-10-12 13:52:37 +0800

    init

---
+++ RayButton/Controls/CircleButton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace RayButton.Controls
+{
+    public class CircleButton:Button
+    {
+        static CircleButton()
+        {
+            FrameworkElement.DefaultStyleKeyProperty.OverrideMetadata(typeof(CircleButton), new FrameworkPropertyMetadata(typeof(CircleButton)));
+        }
+    }
+}
